3b01a3bd3be4ba74ee7f6e27bdf4f6aed2b5c28a
Yuriy Syrovetskiy <cblp@cblp.su>
2015-07-25 15:59:23 +0300

    [+] 1 pipe in shell, 2 pipes in shell

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,16 @@ import Criterion.Main ( bench, bgroup, defaultMain, nfIO )
 runTrue :: IO ()
 runTrue = runIO "true"
 
+run1PipeInShell :: IO ()
+run1PipeInShell = runIO "seq 1 1000 | wc -l >/dev/null"
+
+run2PipesInShell :: IO ()
+run2PipesInShell = runIO "yes | head -n 1000 | wc -l >/dev/null"
+
 main :: IO ()
 main = defaultMain
-    [ bgroup "HSH"  [ bench "true" $ nfIO runTrue
+    [ bgroup "HSH"  [ bench "true"              $ nfIO runTrue
+                    , bench "1 pipe in shell"   $ nfIO run1PipeInShell
+                    , bench "2 pipes in shell"  $ nfIO run2PipesInShell
                     ]
     ]
