a5c70216cd077d81b3a2dcf837aaabaeec38fd80
Yuriy Syrovetskiy <cblp@cblp.su>
2015-07-25 16:33:37 +0300

    [+] assertEqual

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,26 @@
-import HSH            ( runIO )
-import Criterion.Main ( bench, bgroup, defaultMain, nfIO )
+module Main               ( main ) where
+
+import Criterion.Main     ( bench, bgroup, defaultMain, nfIO )
+import Data.Functor       ( (<$>) )
+import Data.Monoid        ( (<>) )
+import Data.String.Utils  ( strip )
+import HSH                ( run, runIO )
+import Test.HUnit         ( assertEqual )
 
 runTrue :: IO ()
 runTrue = runIO "true"
 
 run1PipeInShell :: IO ()
-run1PipeInShell = runIO "seq 1 1000 | wc -l >/dev/null"
+run1PipeInShell = do  let n = 1000000 :: Int
+                          cmd = "seq 1 " <> show n <> " | wc -l"
+                      result <- read . strip <$> run cmd
+                      assertEqual "run1PipeInShell result" n result
 
 run2PipesInShell :: IO ()
-run2PipesInShell = runIO "yes | head -n 1000 | wc -l >/dev/null"
+run2PipesInShell = do let n = 1000000 :: Int
+                          cmd = "yes | head -n " <> show n <> " | wc -l"
+                      result <- read . strip <$> run cmd
+                      assertEqual "run2PipesInShell result" n result
 
 main :: IO ()
 main = defaultMain
